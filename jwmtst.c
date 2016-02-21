0a84e85c49681687f18361692054b331951c9ed2
ander <andreyrojojin@yandex.ru>
2016-02-21 20:52:43 +0300

    jwmtst не пишет в файлы, а выдает все на выход.
    Нужно для динамического меню

--- jwmtst.c
+++ jwmtst.c
@@ -5,9 +5,11 @@
 #include <sys/stat.h>
 #include <errno.h>
 
-#define CAT_MAX 15		// максимум категорий
-#define KW_MAX 15		// максимум ключевых слов, включая имя файла, в категории
-#define VLEN_MAX 96			// максимальная длина строки
+#define CAT_MAX 10		// максимум категорий
+#define KW_MAX 16		// максимум ключевых слов, включая название и иконку, в категории
+#define FILE_IN_CAT_MAX 50	// максимум пунктов меню в категории
+#define DTENTRY_MAX 300		// максимум desktop файлов на входе
+#define VLEN_MAX 96			// максимальная длина строки в desktop файле
 
 typedef struct {
 	char *name;
@@ -18,8 +20,6 @@ typedef struct {
 
 // ищет в начале buf подстроку str, если есть, то все после '=' копирует в динамически выделенную память, адрес помещает в savestr
 int stringsearch(const char *buf, const char *str, char **savestr);
-// проверить наличие директории, если нет, то создать.
-int dircheck(const char *dirname);
 // разобрать desktop файл, поместить в структуру взятые из него name, icon, exec,  categories
 int parsefile(const char *filename, dtentryT *rezult);
 // функция для сортировки по именам
@@ -28,46 +28,36 @@ int compar(const void *a, const void *b);
 int main(int argc, char **argv)
 {
 	char *appdirs[] = { "/usr/share/applications", "/usr/local/share/applications", "/home/live/.local/share/applications", NULL };
-	char *outdir = "/home/live/.jwm/menu";
-	char *categorykw[CAT_MAX][KW_MAX] =  {  // сначала имя файла, куда складываем, потом ключевые слова категории
-		{"System", "System","Monitor","Security","Core", NULL },
-		{"Settings", "Settings","DesktopSettings","HardwareSettings","Setup","PackageManager","Desktop","Screensaver","Accessibility", NULL },
-		{"Utility", "Utility","Viewer","Profiling","Translation","GUIDesigner","Archiving","TerminalEmulator","Shell","File", NULL},
-		{"Development", "Development","Building","Debugger","IDE", NULL},
-		{"Graphic", "Graphic","Photography","Presentation","Chart", NULL},
-		{"Office", "Office","Document","WordProcessor","WebDevelopment","TextEditor","Dictionary","Calculat","Finance","Spreadsheet","ProjectManagement","Personal","Calendar","ContactManagement", NULL},
-		{"Internet", "Internet","WebBrowser","Email","News","InstantMessaging","Telephony","IRCClient","FileTransfer","P2P","Network","Dialup","HamRadio","RemoteAccess", NULL},
-		{"Multimedia", "Video","Player","Music","Audio","Midi","Mixer","Sequencer","Tuner","TV","DiskBurning", NULL},
-		{"Game", "Game","Amusement","RolePlaying","Simulation", NULL},
+	
+	char *categorykw[CAT_MAX][KW_MAX] =  {  // сначала имя категории, потом иконка, потом ключевые слова категории
+		{"Система", "applications-system", "System","Monitor","Security","Core", NULL },
+		{"Настройки", "preferences-desktop", "Settings","DesktopSettings","HardwareSettings","Setup","PackageManager","Desktop","Screensaver","Accessibility", NULL },
+		{"Инструменты", "applications-accessories", "Utility","Viewer","Profiling","Translation","GUIDesigner","Archiving","TerminalEmulator","Shell","File", NULL},
+		{"Разработка", "applications-development", "Development","Building","Debugger","IDE", NULL},
+		{"Графика", "applications-graphics", "Graphic","Photography","Presentation","Chart", NULL},
+		{"Офис", "applications-office", "Office","Document","WordProcessor","WebDevelopment","TextEditor","Dictionary","Calculat","Finance","Spreadsheet","ProjectManagement","Personal", "Calendar","ContactManagement", NULL},
+		{"Интернет", "applications-internet", "Internet","WebBrowser","Email","News","InstantMessaging","Telephony","IRCClient","FileTransfer","P2P","Network","Dialup","HamRadio","RemoteAccess", NULL},
+		{"Мультимедиа", "applications-multimedia", "Video","Player","Music","Audio","Midi","Mixer","Sequencer","Tuner","TV","DiskBurning", NULL},
+		{"Игры", "applications-games", "Game","Amusement","RolePlaying","Simulation", NULL},
 		{ NULL }
-																		};  // массив имен файлов категорий и ключевых слов к ним
-	FILE *outfiles[CAT_MAX];    // массив файловых указателей категорий
+																		};  // массив категорий и ключевых слов к ним
 	int catIndex, wordIndex;
 	char **appdir;
 	DIR *dir;
 	struct dirent *entry;
 	char buf[80];
-	char outfname[64];
 	int len, space;
-	dtentryT dtentry[300];
+	dtentryT dtentry[DTENTRY_MAX];
+	dtentryT **catentry[CAT_MAX]; // здесь будут указатели на массивы, каждый для своей категории. В массивах указатели на разобранные файлы.
+	int catend[CAT_MAX];  // индекс конца каждого из массивов
 	int entryIndex=0;
 	int entryIndexMax;
 	
-	if( ! dircheck(outdir) ) return 1;
-	outfname[0]=0;
-	strncat(outfname, outdir, sizeof(outfname) - 2 );
-	strcat(outfname, "/");
-	len = strlen(outfname);
-	space = sizeof(outfname) - len - 1;
 	for(catIndex=0; catIndex<CAT_MAX; catIndex++){       // по всем категориям
 		if( categorykw[catIndex][0] == NULL ) break;   // если пусто, категории кончились
-		outfname[len] = 0;			// убрали предыдущее имя файла
-		strncat(outfname, categorykw[catIndex][0], space); // записали новое
-		if( (outfiles[catIndex] = fopen(outfname, "w")) != 0 )
-			fprintf(outfiles[catIndex], "<JWM>\n");
-		else
-			perror(outfname);
-	} // пооткрывали сразу все файлы, потом в конце таким же циклом надо закрыть
+		catentry[catIndex] = calloc(FILE_IN_CAT_MAX, sizeof(dtentryT*) );
+		catend[catIndex] = 0;  // пока там пусто
+	} // распределили память под массивы категорий
 	
 	for( appdir=appdirs; *appdir != NULL; appdir++){  // по всем каталогам с *.desktop файлами
 		dir = opendir(*appdir);
@@ -82,12 +72,16 @@ int main(int argc, char **argv)
 				buf[len]=0;
 				strncat(buf,entry->d_name, space);
 				entryIndex += parsefile( buf, dtentry + entryIndex);	// разобрали файл, если успешно, заполнять будем следующую ячейку массива
+				if( entryIndex == DTENTRY_MAX ){
+					fprintf(stderr, "Too many desktop files\n");
+					return 1;  // не помещаются все файлы, аварийный выход
+				}
 			} /* while readdir */
 			closedir(dir);
 		} /* if(dir) */
 	} // по всем каталогам с *.desktop файлами
-	entryIndexMax = entryIndex;
-
+	entryIndexMax = entryIndex; // запомнили, до какого индекса массив заполнен
+	
 	qsort(dtentry, entryIndexMax, sizeof(dtentryT) , compar );  // рассортировали, то что выбрали из файлов
 
 	for( entryIndex = 0; entryIndex < entryIndexMax; entryIndex++ ){  // Раскидываем сортированные данные по категориям
@@ -96,50 +90,39 @@ int main(int argc, char **argv)
 			for( wordIndex=1; wordIndex<KW_MAX; wordIndex++ ){     // по всем ключевым словам категории
 				if( categorykw[catIndex][wordIndex] == NULL ) break;         // ключевые слова закончились, выходим из их перебора
 				if ( strstr( dtentry[entryIndex].categories, categorykw[catIndex][wordIndex] ) ){
-					// если категория нашлась, вывели в соответствующий файл
-					fprintf(outfiles[catIndex], "\t<Program label=\"%s\" icon=\"%s\">%s</Program>\n", \
-														dtentry[entryIndex].name, dtentry[entryIndex].icon, dtentry[entryIndex].exec);
+					// если категория нашлась, записали адрес ячейки в соотвествующий массив, если там место не кончилось
+					if( catend[catIndex] == FILE_IN_CAT_MAX ){
+						fprintf(stderr, "Too many entries in category:%s\n", categorykw[catIndex][0]);
+						break;  // не помещаются все ссылки в категорию, придется пропустить
+					}
+					catentry[catIndex][catend[catIndex]] = dtentry + entryIndex;
+					catend[catIndex]++;		// пометили, что уровень заполнения увеличился
 					break;								// и пошли на следующую категорию
 				}
 			}
 		}
+	}
+	
+	printf("<JWM>\n");
+	for(catIndex=0; catIndex<CAT_MAX; catIndex++){       // по всем категориям
+		if( categorykw[catIndex][0] == NULL ) break;   // если пусто, категории кончились
+		printf("<Menu label=\"%s\" icon=\"%s\">\n", categorykw[catIndex][0], categorykw[catIndex][1]);
+		for(entryIndex=0; entryIndex < catend[catIndex]; entryIndex++){
+			printf("\t<Program label=\"%s\" icon=\"%s\">%s</Program>\n", \
+					catentry[catIndex][entryIndex]->name, catentry[catIndex][entryIndex]->icon, catentry[catIndex][entryIndex]->exec);
+		}
+		printf("</Menu>\n");
+		free(catentry[catIndex]);
+	} // вывели категории и освободили память массива категории
+	printf("</JWM>\n");
+	for( entryIndex = 0; entryIndex < entryIndexMax; entryIndex++ ){
 		free(dtentry[entryIndex].name);	// освободили созданные strdup строки
 		free(dtentry[entryIndex].icon);
 		free(dtentry[entryIndex].exec);
 		free(dtentry[entryIndex].categories);
 	}
-
-	for(catIndex=0; catIndex<CAT_MAX; catIndex++){       // по всем категориям
-		if( categorykw[catIndex][0] == NULL ) break;   // если пусто, категории кончились
-		fprintf(outfiles[catIndex], "</JWM>\n");
-		fclose(outfiles[catIndex]);
-	} // позакрывали все файлы
 	return 0;
 } /* main() */
-
-// проверить наличие директории, если нет, то создать.
-int dircheck(const char *dirname){
-	DIR *dir;
-	char dncopy[64];
-	char *sl;
-
-	if( (dir = opendir(dirname)) != 0 ){
-		closedir(dir);						//нормально открылась - закрыть и ничего не делать
-		return 1;
-	}else{										// не открылась - проверить, есть ли предшествующая, а потом создать
-		if( errno != ENOENT ){ perror(dirname); return 0; }	// не открылась не по причине отсутствия
-		strncpy(dncopy, dirname, sizeof(dncopy) - 1);
-		dncopy[sizeof(dncopy) - 1] = 0;
-		if( (sl = strrchr(dncopy, '/')) != NULL ) *sl = 0;	//  подрезали имя по последний слеш
-		else return 0;													// нечего подрезать
-		if( ! dircheck(dncopy) ) return 0; // предшествующую создать не получилось
-		if( mkdir(dirname, 0755) == -1 ){
-			perror(dirname);				// не получилось создать
-			return 0;
-		}
-		return 1;
-	}
-}
 
 // ищет в начале buf подстроку str, если есть, то все после '=' копирует в динамически выделенную память, адрес помещает в savestr
 int stringsearch(const char *buf, const char *str, char **savestr){
